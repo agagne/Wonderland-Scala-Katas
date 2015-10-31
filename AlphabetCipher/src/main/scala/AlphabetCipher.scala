a7a67d842e20a462a7424d5897c922629d436315
agagne <agagne@gmail.com>
2015-10-31 11:06:27 -0400

    - encode working - decode working
    * TODO: can encode be solved with no for loop
    * TODO: can decode be solved with no for loop
    * TODO: decipher  - find shortest repetative key

--- AlphabetCipher/src/main/scala/AlphabetCipher.scala
+++ AlphabetCipher/src/main/scala/AlphabetCipher.scala
@@ -1,14 +1,41 @@
 object AlphabetCipher {
-  def encode(keyword: String, message: String): String = {
-    keyword
+  private final val Alphabet = "abcdefghijklmnopqrstuvwxyz"
+  
+  def encode(keyword: String, message: String): String = { //TODO: can this be done without the for loop?
+    var encoded = ""
+    for(i <-0 until message.length){
+     val m = Alphabet.indexOf(message.charAt(i))
+     val k = Alphabet.indexOf(keyword.charAt(i%keyword.length()))
+     val e = (m+k)%Alphabet.length()
+     encoded += Alphabet.charAt(e)     
+    }
+    encoded
   }
 
-  def decode(keyword: String, message: String): String = {
-    ??? //keyword
+  def decode(keyword: String, message: String): String = { //TODO: can this be done without the for loop?
+    var decoded = ""
+    for(i <-0 until message.length){
+     val m = Alphabet.indexOf(message.charAt(i))
+     val k = Alphabet.indexOf(keyword.charAt(i%keyword.length()))
+     val e =((m - k) + 26 )%Alphabet.length()
+     decoded += Alphabet.charAt(e)     
+    }
+    decoded
   }
-
+  
   def decipher(cipher: String, message: String): String = {
-    ??? //cipher
+    var deciphered = ""
+    for(i <-0 until message.length){
+     val m = Alphabet.indexOf(message.charAt(i))
+     val e  =Alphabet.indexOf(cipher.charAt(i))
+     val k = ((e - m) + 26 )%Alphabet.length()
+     deciphered += Alphabet.charAt(k)     
+    }
+    findShortestRepeat(deciphered)
+  }
+  def findShortestRepeat(repeats : String): String = {
+    ???
+    //TODO: this is similar to a problem from the bioinformatics algorithm class you took
   }
 
 }
--- AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
+++ AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
@@ -1,13 +1,23 @@
-import AlphabetCipher._
 import org.scalatest._
+
+import AlphabetCipher.decipher
+import AlphabetCipher.decode
+import AlphabetCipher.encode
+import AlphabetCipher.findShortestRepeat
 
 class AlphabetCipherSpec extends FlatSpec with Matchers {
   "encode" should "encode given a secret keyword" in {
+    assert(encode("a","k") == "k")
+    assert(encode("i","e") == "m")
+    assert(encode("v","m") == "h")
     assert(encode("vigilance","meetmeontuesdayeveningatseven") == "hmkbxebpxpmyllyrxiiqtoltfgzzv")
     assert(encode("scones","meetmebythetree") == "egsgqwtahuiljgs")
   }
 
   "decode" should "decode an cyrpted message given a secret keyword" in {
+    assert(decode("a","k") == "k")
+    assert(decode("i","m") == "e")
+    assert(decode("v","h") == "m")
     assert(decode("vigilance","hmkbxebpxpmyllyrxiiqtoltfgzzv") == "meetmeontuesdayeveningatseven")
     assert(decode("scones","egsgqwtahuiljgs") == "meetmebythetree")
 
@@ -16,5 +26,11 @@ class AlphabetCipherSpec extends FlatSpec with Matchers {
   "decipher" should "extract the secret keyword given an encrypted message and the original message" in {
     assert(decipher("opkyfipmfmwcvqoklyhxywgeecpvhelzg", "thequickbrownfoxjumpsoveralazydog") == "vigilance")
     assert(decipher("hcqxqqtqljmlzhwiivgbsapaiwcenmyu", "packmyboxwithfivedozenliquorjugs") == "scones")
+  }
+  
+  "findShortestRepeat" should "find the shorted repeated word" in {
+    assert(findShortestRepeat("vigilance") == "vigilance")
+    assert(findShortestRepeat("vigilancevigilance") == "vigilance")
+    assert(findShortestRepeat("abcdabcdab") == "abcd")
   }
 }
